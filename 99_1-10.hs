ad92b46d0d5a49929564467fb346cd8d97548f63
Shivam Tripathi <shivam.flash@gmail.com>
2015-03-03 23:28:42 +0530

    Solve few more problems

--- 99_1-10.hs
+++ 99_1-10.hs
@@ -29,3 +29,30 @@ listReverse (x:xs) = (listReverse xs) ++ [x]
 -- Problem 6
 listpalindrome :: Eq a => [a] -> Bool
 listpalindrome list = list == (listReverse list) 
+
+-- Problem 7
+-- data NestedList a = Elem a| List [NestedList a]
+-- flatten :: NestedList a -> [a] 
+-- flatten Elem a  = [a]
+-- flatten (List ((Elem x):xs)) = [x] ++ (flatten xs) 
+-- [1:2:[3:4:[]]:5:6:[7:8:9:[]]]
+-- flatten ((Elem x):xs) = x : (map flatten xs)  
+
+-- Problem 8 
+{-- This works too, but it's brute force :P
+compress :: Eq a =>[a] -> [a]
+compress [] = []
+compress (x:xs) = x : (callagain (x:xs) 1)
+
+callagain :: Eq a =>[a] -> Int -> [a]
+callagain [] _ = []
+callagain xs index =  if (index < length xs ) then if ((xs!!index) /= ( xs!! (index-1))) then (xs!!index) : ( callagain xs (index + 1)) else ( callagain xs (index + 1)) else []
+
+--}
+compress :: Eq a => [a] -> [a]
+compress [] = []
+compress (x:xs) = x : (callagain xs x)
+callagain [] _ = []
+callagain (x:xs) y = if ( x == y ) then (callagain xs) y else (x : callagain xs x)
+
+-- Problem 9
